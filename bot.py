f5e4f96035df88eee328fef43f9a2bf435e51011
Hugo Greenwood <greenwoodhugo@gmail.com>
2023-05-30 20:45:28 +0100

    .

--- bot.py
+++ bot.py
@@ -1,85 +1,68 @@
 import os
-import getpass 
 import openai
+import getpass
 import spotipy
-import random
+from flask import request, jsonify
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from random import sample
-
-
 
 # Get OpenAI API key
 openai.api_key = getpass.getpass("Please enter your OpenAI API key: ")
 
-def define_spotify():
-    print("1: Enter your Spotify API details\n2: Log in to your Spotify account")
-    spotify_option = input("Input (1 or 2): ")
 
-    if spotify_option not in ['1', '2']:
-        print("Must input either 1 or 2")
-        return define_spotify()
-
-    if spotify_option == '1':
-        client_id = getpass("Please enter your Spotify Client ID: ")
-        client_secret = getpass("Please enter your Spotify Client Secret: ")
-        auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-
-    elif spotify_option == '2':
-        print("Please log in to your Spotify account.")
-        scope = "playlist-read-private playlist-read-collaborative user-library-read playlist-modify-public playlist-modify-private"
-        auth_manager = SpotifyOAuth(scope=scope, open_browser=True)
-
-    return spotipy.Spotify(auth_manager=auth_manager)
-
-spotify = define_spotify()
-
-
-# %%
-
-def get_user_tracks():
-    results = spotify.current_user_saved_tracks()
-    return [track['track']['id'] for track in results['items']]
-
-def get_related_tracks(user_tracks):
-    related_tracks = []
-    for track in user_tracks:
-        related = spotify.artist_top_tracks(spotify.track(track)['artists'][0]['id'])
-        related_tracks.extend([t['id'] for t in related['tracks']])
-    return related_tracks
-
-def gpt_prompt(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=60
-    )
-    return response.choices[0].text.strip()
-
-def create_playlist(prompt):
-    # Generate playlist name using GPT-4
-    playlist_name = gpt_prompt(prompt)
+if (openai.api_key == ""):
+    print("using default key")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     
+    
+def handle_bot(data):
+    def define_spotify():
+        client_id = os.getenv("SPOTIFY_CLIENT_ID")
+        client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+        auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+        return spotipy.Spotify(auth_manager=auth_manager)
+
+    spotify = define_spotify()
+
+    def get_user_tracks():
+        results = spotify.current_user_saved_tracks()
+        return [track['track']['id'] for track in results['items']]
+
+    def get_related_tracks(user_tracks):
+        related_tracks = []
+        for track in user_tracks:
+            related = spotify.artist_top_tracks(spotify.track(track)['artists'][0]['id'])
+            related_tracks.extend([t['id'] for t in related['tracks']])
+        return related_tracks
+
+    def gpt_prompt(prompt):
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=60
+        )
+        return response.choices[0].text.strip()
+
+    # Generate playlist name using GPT-4
+    playlist_name = gpt_prompt(data['prompt'])
+
     # Get user's tracks
     user_tracks = get_user_tracks()
-    
+
     # Get related tracks
     related_tracks = get_related_tracks(user_tracks)
-    
+
     # Get intersection of user's tracks and related tracks
     common_tracks = list(set(user_tracks) & set(related_tracks))
-    
+
     # Create new playlist
-    playlist = spotify.user_playlist_create(os.getenv("spotifyUSERNAME"), playlist_name)
-    
+    playlist = spotify.user_playlist_create(os.getenv("SPOTIFY_USERNAME"), playlist_name)
+
     # Add half from user's tracks and half from related tracks
     half_length = len(common_tracks) // 2
     tracks_to_add = common_tracks[:half_length] + related_tracks[:half_length]
-    
+
     # Add tracks to the playlist
     spotify.playlist_add_items(playlist['id'], tracks_to_add)
 
-# Prompt for creating the playlist
-prompt = "Create a playlist for coding and focusing."
-create_playlist(prompt)
-
-
+    return {'status': 'Playlist created successfully'}
--- server.py
+++ server.py
@@ -1,62 +1,25 @@
+from flask import Flask, request,jsonify,render_template_string,session, redirect, url_for
+from flask_session import Session
+import redis
+import bot  # assuming bot.py is in the same directory
+from bot import handle_bot 
+from datetime import timedelta
+
+app = Flask(__name__)
+
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
 
 
+@app.route('/bot', methods=['POST'])
+def handle_request():
+    data = request.json
+    response = handle_bot(data)
+    return jsonify(response)
 
-import os
-import spotipy
-import openai
-from spotipy.oauth2 import SpotifyOAuth
-from random import sample
 
-# Setup OpenAI API
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-# Setup Spotify API
-scope = "user-library-read playlist-modify-public"
-username = os.getenv("SPOTIFY_USERNAME")
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=username))
-
-def get_user_tracks():
-    results = spotify.current_user_saved_tracks()
-    return [track['track']['id'] for track in results['items']]
-
-def get_related_tracks(user_tracks):
-    related_tracks = []
-    for track in user_tracks:
-        related = spotify.artist_top_tracks(spotify.track(track)['artists'][0]['id'])
-        related_tracks.extend([t['id'] for t in related['tracks']])
-    return related_tracks
-
-def gpt_prompt(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=60
-    )
-    return response.choices[0].text.strip()
-
-def create_playlist(prompt, username):
-    # Generate playlist name using GPT-4
-    playlist_name = gpt_prompt(prompt)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
     
-    # Get user's tracks
-    user_tracks = get_user_tracks()
-    
-    # Get related tracks
-    related_tracks = get_related_tracks(user_tracks)
-    
-    # Get intersection of user's tracks and related tracks
-    common_tracks = list(set(user_tracks) & set(related_tracks))
-    
-    # Create new playlist
-    playlist = spotify.user_playlist_create(username, playlist_name)
-    
-    # Add half from user's tracks and half from related tracks
-    half_length = len(common_tracks) // 2
-    tracks_to_add = common_tracks[:half_length] + related_tracks[:half_length]
-    
-    # Add tracks to the playlist
-    spotify.playlist_add_items(playlist['id'], tracks_to_add)
-
-# Prompt for creating the playlist
-prompt = "Create a playlist for coding and focusing."
-create_playlist(prompt, username)
